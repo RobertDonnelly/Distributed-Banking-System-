2ee748aab48c64c2733a8e2fe2d84b3e8889432a
RobertDonnelly <37175369+RobertDonnelly@users.noreply.github.com>
2021-02-24 19:24:34 +0000

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd bank-api-service; mvn clean package -DskipTests=false; cd ..
+cd user-notification-service; mvn clean package; cd ..
+cd reporting-service; mvn clean package; cd ..
+cd account-manager; mvn clean package; cd ..
